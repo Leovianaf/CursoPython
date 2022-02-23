7a9a6a86a6352200cd0f7b00e6dc97db38066783
Leonardo da Silva Viana Filho <leonardo_viana@leonardo.viana-Nitro-AN515-54>
2022-02-23 14:54:00 -0300

    aula1

--- secao4/aula1/main.py
+++ secao4/aula1/main.py
@@ -1,1 +1,10 @@
-print('Hello World')
+from pessoa import  Pessoa
+
+p1 = Pessoa('Luiz',29)
+p2 = Pessoa ('João', 32)
+
+p1.comer('pizza')
+p2.falar('sexo')
+p1.pararComer()
+print(p1.getAnoNascimento())
+print(p2.getAnoNascimento())
